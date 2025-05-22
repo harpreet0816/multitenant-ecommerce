a2c995aad5ff976c7096c9443a7a084941ddd62a
harpreet0816 <harpreet8730@gmail.com>
2025-05-22 11:17:36 +0530

    started search filter functionality

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,14 +1,6 @@
-import configPromise from "@payload-config";
-import { getPayload } from "payload";
 
 // /admin for cms
 export default async function Home() {
-  const payload = await getPayload({
-    config: configPromise,
-  });
 
-  const data = await payload.find({
-    collection: "categories",
-  });
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return <div>Home Component</div>;
 }
